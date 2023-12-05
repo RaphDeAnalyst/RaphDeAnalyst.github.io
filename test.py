1be1efa248928b00320337a3d62ccb7da62a65f2
RaphDeAnalyst <raphandy007@gmail.com>
2023-12-05 13:47:42 +0100

    Initial Commit

---
+++ test.py
@@ -0,0 +1,11 @@
+double = lambda x: x*2
+print(double(2))
+
+Multiply = lambda x, y: x*y
+print(Multiply(2,6))
+
+fullname = lambda x, y: x + " " + y
+print(fullname("matthew", "Raphael"))
+
+age = lambda age: True if age >= 18 else False
+print(age(17))
